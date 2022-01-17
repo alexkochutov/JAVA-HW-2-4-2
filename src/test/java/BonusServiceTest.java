0b88daabee92f3d6716cf15cfea464c7b21c4c32
alexkochutov <alex.kochutov@gmail.com>
2022-01-17 23:23:53 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1)
+    void shouldCalculate(String name, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
